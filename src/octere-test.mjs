4b5d8d720aa9cefd53fd2d3326d24f347912a449
d3x0r <d3x0r@no-reply.github.com>
2020-04-24 09:13:29 -0700

    Update test to pull actual octree-able data

--- src/octere-test.mjs
+++ src/octere-test.mjs
@@ -1,6 +1,30 @@
 
 import {OctereTree} from "./octeretree.mjs";
 
-var tree = OctereTree( [], [100, 100, 100] );
+import * as X from "./testdata.mjs";
+var meshes = X.createTestData();
+
+var now = Date.now();
+var then = now;
+var mesh = meshes.Sphere();
+console.log( "Magic?", Object.keys(meshes), mesh );
+now = Date.now();
+var tree = OctereTree( mesh.data, mesh.dims );
+console.log( "MIPPING took", Date.now()-now );
 var r = tree.raycast( [0,0,0], [Math.sqrt(1/3),Math.sqrt(1/3),Math.sqrt(1/3)] );
-console.log( "Path result:", r.toString(8));
+console.log( "Path result:", r);
+
+
+
+mesh = meshes.Terrain();
+console.log( "terrain:", mesh.dims );
+now = Date.now();
+tree = OctereTree( mesh.data, mesh.dims );
+console.log( "MIPPING took", Date.now()-now );
+
+
+mesh = meshes['Sine Waves']();
+console.log( "Sine Waves:", mesh.dims );
+now = Date.now();
+tree = OctereTree( mesh.data, mesh.dims );
+console.log( "MIPPING took", Date.now()-now );
